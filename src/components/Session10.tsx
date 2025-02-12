5a4ed3a0317ca8bc1bd79a282a3e1331f7231427
irlanreis <xuiexpress@gmail.com>
2025-02-12 15:08:33 -0400

    Fix: correções gerais

--- src/components/Session10.tsx
+++ src/components/Session10.tsx
@@ -43,6 +43,32 @@ const Session10 = () => {
     infinite: true,
     autoplay: true,
     autoplaySpeed: 3000,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
